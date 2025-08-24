1a6dd622a322b80abc9eafa53fb3ac52066e9457
ikets <maky.ito@gmail.com>
2025-08-24 22:24:44 +0900

    reorganize dir structure

--- sh/reproduce_results.sh
+++ sh/reproduce_results.sh
@@ -3,7 +3,3 @@
 python3 test.py --exp_dir exp/v1 --device cpu
 python3 test_baseline.py --config_path config/baseline_v1.yaml --exp_dir exp_baseline
 python3 plot_results.py --exp_dir_proposed exp/v1 --exp_dir_baseline exp_baseline --out_dir figure
-
-python3 test.py --exp_dir exp/v1_linear --device cpu
-python3 test.py --exp_dir exp/v1_wo-freq-cond --device cpu
-python3 plot_results_ablation_only.py
